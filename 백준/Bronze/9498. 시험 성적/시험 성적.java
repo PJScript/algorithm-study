2bca9abb38006d06ebbcad0d98b0b7322428dfe7
Junsu Park <74460103+PJScript@users.noreply.github.com>
2023-11-30 09:12:34 +0900

    [Bronze V] Title: 시험 성적, Time: 208 ms, Memory: 17752 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int score = scanner.nextInt();
+
+
+        if (score <= 100 && score >= 90) {
+            System.out.print("A");
+        } else if (score <= 89 && score >= 80) {
+            System.out.print("B");
+        } else if (score <= 79 && score >= 70) {
+            System.out.print("C");
+        } else if (score <= 69 && score >= 60) {
+            System.out.print("D");
+        } else {
+            System.out.println("F");
+        }
+
+
+    }
+
+}
