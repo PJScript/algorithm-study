72ecbb2dc02eb3ebc1721d808a71ccad553637a9
박준수 <74460103+PJScript@users.noreply.github.com>
2023-12-11 17:44:11 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 124 ms, Memory: 14088 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        // 빅오 표기법에 관한 내용이다. 문제를 이해하는게 더 어려운 문제였다.
+        System.out.println(1);
+        System.out.println(0);
+    }
+}
